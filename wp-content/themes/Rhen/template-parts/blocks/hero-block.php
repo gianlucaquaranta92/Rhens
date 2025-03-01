a42f3c177b3712408a8aa8c0a3c4171761560239
gianluca <gianlucaquaranta92@gmail.com>
2025-03-01 22:19:43 +0100

    commit the theme

---
+++ wp-content/themes/Rhen/template-parts/blocks/hero-block.php
@@ -0,0 +1,36 @@
+<?php
+// Retrieve ACF fields and store them in variables, checking if each field exists and is not empty
+$hero_subheading = !empty(get_field('hero_top_title')) ? get_field('hero_top_title') : '';
+$hero_heading = !empty(get_field('hero_title')) ? get_field('hero_title') : '';
+$hero_description = !empty(get_field('hero_subtitle')) ? get_field('hero_subtitle') : '';
+$hero_button_url = !empty(get_field('hero_button_link')) ? get_field('hero_button_link') : '';
+$hero_button_text = !empty(get_field('hero_button_text')) ? get_field('hero_button_text') : '';
+$hero_image = !empty(get_field('hero_image')) ? get_field('hero_image') : '';
+$hero_award_text = !empty(get_field('hero_award_text')) ? get_field('hero_award_text') : '';
+?>
+
+<section class="hero-section relative">
+    <img style="z-index: -1" class=" absolute top-0 left-0 w-full h-full object-cover " src="<?php echo esc_url($hero_image); ?>" alt="Hero Image">
+      <div style="z-index: -1; opacity: 50% ;" class="z-1 absolute top-0 left-0 w-full h-full bg-black opacity-[30%]"></div>          
+    <div class="page-width h-full">
+        <div class="flex flex-row items-center h-full">
+            <div class="flex flex-col gap-4 grow w-full text-white text-center">
+                <h4 class="text-primary font-bold"><?php echo esc_html($hero_subheading); ?></h4>
+                <h1><?php echo esc_html($hero_heading); ?></h1>
+                <div class="text-xl  font-bold"><?php echo esc_html($hero_description); ?></div>
+
+                <?php if ($hero_button_url && $hero_button_text): ?>
+                    <div>
+                        <a class="button w-max" href="<?php echo esc_url($hero_button_url); ?>">
+                            <?php echo esc_html($hero_button_text); ?>
+                        </a>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <div class="flex justify-end grow">
+            
+            </div>
+        </div>
+    </div>
+</section>
+
---
+++ wp-content/themes/Rhen/template-parts/blocks/services-block.php
@@ -0,0 +1,29 @@
+<section class="services-block">
+    <div class="page-width">
+        <div>
+            <div class="flex flex-row flex-wrap items-center gap-4">
+                <?php if( have_rows('services_repeater') ): ?>
+                    <?php foreach( get_field('services_repeater') as $service ): 
+                        $service_title = $service['service_title'];
+                        $service_description = $service['service_description'];
+                        $service_image = $service['service_image']; 
+                        $service_link = $service['service_url'];
+                    ?>
+                        <a href="<?php echo esc_url($service_link); ?>" class="text-white aspect-square min-h-[200px] overflow-hidden group relative shadow-md p-8 grow max-w-[32%] w-full flex justify-end flex-col gap-4  transition-transform duration-300 h-[]">
+                            <div class="overlay-container">
+                                <div class="red-overlay ">
+                                </div>
+                                <h4 class="z-10 text-3xl  duration-300"><?php echo esc_html($service_title); ?></h4>
+                                <p class="text-lg" ><?php echo esc_html($service_description); ?></p>
+                            </div>
+                    
+                            <img class="absolute top-0 left-0 w-full h-full z-[-10]  duration-300 object-cover " src="<?php echo esc_html($service_image); ?>" alt="hero-image" />
+                            <p class="read-more z-10 text-xl  font-bold duration-300">Läs mer →</p>
+                        </a>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
+
